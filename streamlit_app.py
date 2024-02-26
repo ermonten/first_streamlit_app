07596ef6036440668afd8914fc4ad37ea8ce6484
ermonten <58109253+ermonten@users.noreply.github.com>
2024-02-26 16:54:45 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,20 +46,6 @@ try:
 except URLError as e:
   streamlit.error()
   
-
-#streamlit.write('The user entered ', fruit_choice)
-
-
-
-
-#streamlit.text(fruityvice_response.json()) #just writes the data to the screen
-
-# write your own comment -what does the next line do? 
-
-# write your own comment - what does this do?
-
-
-
 #don't run anything past here 
 #streamlit.stop()
 
@@ -69,7 +55,9 @@ def get_fruit_load_list():
     return my_cur.fetchall()
 
 # add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
+
+streamlit.header('View Our Fruit List - Add Your Favorites!')
+if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows =  get_fruit_load_list()
   my_cnx.close()
